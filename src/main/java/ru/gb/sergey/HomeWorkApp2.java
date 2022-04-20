7ed2b2f66267c9210cd45a73d5e4195713a6738f
NewRostov <kim.sergey.rostov@gmail.com>
2022-04-20 03:59:32 +0300

    урок 2, задание 3

--- src/main/java/ru/gb/sergey/HomeWorkApp2.java
+++ src/main/java/ru/gb/sergey/HomeWorkApp2.java
@@ -2,8 +2,11 @@ package ru.gb.sergey;
 
 public class HomeWorkApp2 {
     public static void main(String[] args) {
-        System.out.println(c(2, 6));
-        d(-1);
+        System.out.println(c(2, 6)); // задание 1
+
+        d(-1); //задание 2
+
+        System.out.println(e(-5)); // задание 3
     }
     public static boolean c (int a, int b) {
 
@@ -20,6 +23,10 @@ public class HomeWorkApp2 {
 
     }
 
+    public static boolean e(int a) {
+
+            return a >= 0;
+        }
 
 
 }
