555f16b6135f18a22acd3ca738b1165faea5d418
NewRostov <kim.sergey.rostov@gmail.com>
2022-04-20 03:47:37 +0300

    урок 2, задание 2

--- src/main/java/ru/gb/sergey/HomeWorkApp2.java
+++ src/main/java/ru/gb/sergey/HomeWorkApp2.java
@@ -3,12 +3,23 @@ package ru.gb.sergey;
 public class HomeWorkApp2 {
     public static void main(String[] args) {
         System.out.println(c(2, 6));
-
+        d(-1);
     }
     public static boolean c (int a, int b) {
 
         int summ = a + b;
         return summ > 10 && summ < 20;
+    }
+
+    public static void d(int a) {
+        if (a >= 0) {
+            System.out.println("положительное");
+        } else {
+            System.out.println("отрицательное");
+        }
 
     }
+
+
+
 }
