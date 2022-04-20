0566096dcd8d57c6ddd60c7c868e8fc0a4aeff94
NewRostov <kim.sergey.rostov@gmail.com>
2022-04-20 16:03:15 +0300

    урок 2, задание 5 (задание 5 по всей видимости не решил!!!)

--- src/main/java/ru/gb/sergey/HomeWorkApp2.java
+++ src/main/java/ru/gb/sergey/HomeWorkApp2.java
@@ -8,7 +8,12 @@ public class HomeWorkApp2 {
 
         System.out.println(e(-5)); // задание 3
 
-        str("word", 111); // задание 4
+        str("word", 1); // задание 4
+
+
+        year(104); // задавние 5
+
+
     }
     public static boolean c (int a, int b) {
 
@@ -30,8 +35,7 @@ public class HomeWorkApp2 {
     }
 
 
-    public static void str(String a, int b )
-    {
+    public static void str(String a, int b ) {
         for (int i = 0; i < b; i++)
         {
             System.out.println(a + (i + 1));
@@ -40,8 +44,22 @@ public class HomeWorkApp2 {
 
 
 
+    // високосный год -- ХЗ -- как вычислять?????
+    // если делится на 4 без остатка значит високосный
+    // каждый 100 год не високосный
+    // и каждый 400 високосный
+    public static void year(int a ) {
 
-
+        if (a % 400 == 0 ) {
+            System.out.println("високосный потому что какждый 400-й год");
+        } else if (a % 100 == 0) {
+            System.out.println("не високосный потому что делится на 100");
+        } else if (a % 4 == 0) {
+            System.out.println("  високосный потому что делится на 4");
+        } else {
+            System.out.println("не високосный НЕ делится на 4");
+        }
+    }
 
 
 }
