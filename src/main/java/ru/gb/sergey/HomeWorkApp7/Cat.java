26c81db8e5e6c46f7fb72d3ad13c6a31df8b0464
NewRostov <kim.sergey.rostov@gmail.com>
2022-06-02 16:52:50 +0200

    урок 7 не доделал задания

---
+++ src/main/java/ru/gb/sergey/HomeWorkApp7/Cat.java
@@ -0,0 +1,11 @@
+package ru.gb.sergey.HomeWorkApp7;
+
+public class Cat {
+    private String name;
+    private int appetite;
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite; }
+    public void eat(Plate p) { p.decreaseFood(appetite);
+    }
+}
