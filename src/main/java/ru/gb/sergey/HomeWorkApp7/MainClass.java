cc60849b5017358c942412a716fafc968c7f8f47
NewRostov <kim.sergey.rostov@gmail.com>
2022-06-04 23:18:33 +0200

    урок 7 доделал, помогли. С кодом разобрался

--- src/main/java/ru/gb/sergey/HomeWorkApp7/MainClass.java
+++ src/main/java/ru/gb/sergey/HomeWorkApp7/MainClass.java
@@ -7,11 +7,11 @@ public class MainClass {
                 new Cat("Барсик", 20),
                 new Cat("Рыжик", 30)
         };
-        Plate plate = new Plate(40);
+        Plate plate = new Plate(59);
         for (Cat cat : cats) {
             cat.eat(plate);
             System.out.println(cat.isSatiety() ? cat.getName() + " не голоден" : cat.getName() + " голоден");
-            System.out.println("в тарелке " + plate.getFood() + " еды");
+            System.out.println("В тарелке осталось " + plate.getFood() );
             System.out.println();
         }
     }
